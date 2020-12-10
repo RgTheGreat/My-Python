43c24240ee5c9d82f692cd3f378deefd4312eeb1
Rigved <71826040+RgTheGreat@users.noreply.github.com>
2020-12-10 21:10:06 +0530

    Create python-demo.py

---
+++ python-demo.py
@@ -0,0 +1,55 @@
+#variables
+a = "Hello"
+print(a)
+
+
+#if & else
+if 5 > 3:
+    print("Five is greater than three")
+else:
+    print("Three is greater than five")
+
+#casting variables
+x = int(5)
+y = str(5)
+z = float(5)
+
+print(x)
+print(y)
+print(z)
+
+
+#Get the type of the variable
+
+b = 5
+c = 'Hello'
+
+print(type(a))
+print(type(b))
+
+#legal variables
+myvar = "Hello"
+_my_var = "Hello"
+my_var = "hello"
+MYVAR = "hello"
+
+
+#length - length of string
+a = "Hello, my name is Rigved"
+print(len(a))
+
+
+#looping a string
+for a in "PYTHON":
+  print(a) 
+  
+  
+  
+#String are arrays
+a = "Hello, World!"
+print(a[1])
+
+
+#Check strings 
+txt = "My free python"
+print("free" in txt)
