9a847bda9703e9e2cec2d813f2b63d99713ff946
Programmer <71826040+RgTheGreat@users.noreply.github.com>
2021-01-03 12:11:01 +0530

    Create python-file-3.py

---
+++ python-file-3.py
@@ -0,0 +1,23 @@
+# iter
+
+string1 = "Python"
+iter1 = iter(string1)
+
+print(next(iter1))
+print(next(iter1))
+print(next(iter1))
+print(next(iter1))
+print(next(iter1))
+print(next(iter1))
+
+
+
+
+
+
+
+
+list1 = ["Apple", "Banana", "Orange", "Cherry"]
+
+for i in list1():
+  print(i)
