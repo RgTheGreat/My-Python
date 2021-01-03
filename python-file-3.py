fe399db56fde8fd175e574c9901c98edaacd2989
Programmer <71826040+RgTheGreat@users.noreply.github.com>
2021-01-03 12:13:30 +0530

    Update python-file-3.py

--- python-file-3.py
+++ python-file-3.py
@@ -19,5 +19,12 @@ print(next(iter1))
 
 list1 = ["Apple", "Banana", "Orange", "Cherry"]
 
-for i in list1():
+for x in list1():
+  print(x)
+  
+i = 0
+while i < 6:
   print(i)
+  if i == 4:
+    continue
+    i += 1
