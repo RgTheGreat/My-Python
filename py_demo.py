9ef927abd916f6f9ca29a64470506e987ab8257f
Back-end and front-end programmer <71826040+RgTheGreat@users.noreply.github.com>
2021-01-04 17:39:07 +0530

    Update py_demo.py

--- py_demo.py
+++ py_demo.py
@@ -1,2 +1,12 @@
 # dates
-import 
+import datetime
+
+a = datetime.datetime.now()
+# year:
+print(a.year)
+# only a:
+print(a)
+# what you see on your pc:
+print(a.strftime("%D"))
+# or:
+print(a.strftime("%A  %Y"))
