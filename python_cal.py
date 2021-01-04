fa0393bf84a4dbcdc59c2b72b8d5e36700b60f1e
Back-end and front-end programmer <71826040+RgTheGreat@users.noreply.github.com>
2021-01-04 17:24:24 +0530

    Update and rename python-file1.py to python_cal.py

---
+++ python_cal.py
@@ -0,0 +1,28 @@
+# calculation in py
+
+people = [{
+ "name": Mike,
+ "age": 23,
+ "salary": 100
+},
+{
+ "name": Jack,
+ "age": 23,
+ "salary": 300
+},
+{
+ "name": Mack,
+ "age": 23,
+ "salary": 50
+}]
+
+
+# to print the total salary:
+
+total_salary =  0
+  
+for x people:
+  total_salary = total_salry + x[salary]
+  print(total_salary)
+  
+  
