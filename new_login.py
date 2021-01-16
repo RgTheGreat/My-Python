b675976dcf7cd5fdc244d321087889e463a1842b
Back-end and front-end developer <71826040+RgTheGreat@users.noreply.github.com>
2021-01-16 19:22:34 +0530

    Create new_login.py

---
+++ new_login.py
@@ -0,0 +1,60 @@
+import sys 
+# still in progress 
+
+
+ a = input("Enter your password: ")
+  b = int(input("Enter your age: "))
+
+data = {
+	"username": "rbysre"
+	"password": "e713fGH",
+    "age": 12
+}
+
+
+
+
+
+
+
+
+# if fail:
+class fail:
+  def __init__(self, fail, name):
+    self.fail = fail
+    self.name = name
+
+  def func1(self):
+    print(sys.platform() + "," + self.name + self.fail)
+
+x = fail("Can't Log", data["username"])
+x.func1()
+
+
+
+# If success:
+class succuess:
+    def __init__(s, succuess, name)
+      s.succuess = succuess
+      s.name = name
+    
+    def func2(s):
+      print(sys.platform() + "," + s.name + s.success)
+
+y = main("Loged in!", data["username"])
+y.func2()
+
+
+
+
+
+# lgpy
+if a != data["password"]:
+    
+elif a == data["password"]:
+    success()
+
+
+if b != data["password"]:
+# pull requests needed!
+
