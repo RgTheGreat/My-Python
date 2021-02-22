31e0c30624ee41b39522e8245359b1178073ef7a
RgTheGreat <rigved.bob@gmail.com>
2021-02-22 13:17:13 +0530

    updated the todo app

--- apps/sample_todoapp.py
+++ apps/sample_todoapp.py
@@ -1,12 +1,6 @@
 
 while 1:
+  tasks = []  
   a = input("Enter a name for a task: ")
-  print("1.", a)
-  b = input("Enter a new task : ")
-  if b == 'cut':
-    del a
-  print("1.",a + "\n2.",b)
-  c = input("Enter a new task : ")
-  print("1.",a + "\n2.",b + "\n3.",c)
-  d = input("Enter a new task : ")
-  print("1.",a + "\n2.",b + "\n3.",c + "\n4.",d)
+  tasks.append(a)
+  print(tasks)
