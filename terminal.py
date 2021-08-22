17598104dfc78b3defe7f0fbc742afa4f030457d
RgTheGreat <rigved.bob@gmail.com>
2021-08-22 16:40:26 +0530

    Changed the terminal to step 2

--- terminal.py
+++ terminal.py
@@ -1,2 +1,18 @@
+import pickle
+import os
+import shutil
+commands = {
+    "ls": os.listdir()
+}
+
 username = input("Enter username:")
-print("Your Username is: " + username)
+
+while 1:
+    cmd = input("C/Users/~~ ")
+    if cmd in commands.keys():
+        print(os.listdir())
+
+def savepickle(username):
+    with open("username.pkl", "wb") as userpickle:
+        pickle.dump(username, userpickle)
+
