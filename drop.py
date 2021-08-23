e8c7d49ede18de7593a866bd2321ab3c38c4db7c
Back-end and front-end developer <71826040+RgTheGreat@users.noreply.github.com>
2021-08-23 08:14:10 +0530

    Update drop.py

--- drop.py
+++ drop.py
@@ -1,11 +1,11 @@
 import time
 import random
 
-offers = ["21%", "91%", "87%", "3%", "90%", "10%", "96%", "16%", "54%", "74%", "62%", "98%", "100%", "200%", "14%", "154%", "134%", "199%", "0%"]
+#offers = ["21%", "91%", "87%", "3%", "90%", "10%", "96%", "16%", "54%", "74%", "62%", "98%", "100%", "200%", "14%", "154%", "134%", "199%", "0%"]
 
 companies = ["VUE Shop system", "amazon", "myntra", "google ads", "flipkart", "xbox", "ps5"]
 
-offers_choose = random.choice(offers)
+offers_choose = random.randrange(1, 100)
 
 companies_choose = random.choice(companies)
 
@@ -17,7 +17,7 @@ if drop == "drop":
     time.sleep(3)
     print("decoding prize,")
     time.sleep(2)
-    print("Woo hoo! You got " + offers_choose + " in " + companies_choose + " come again and win new prizes!")
+    print("Woo hoo! You got " + str(offers_choose) + str("%") +" in " + str(companies_choose) + " come again and win new prizes!")
 
 
 
